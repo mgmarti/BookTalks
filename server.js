6509df1d7c1209b1b6485305a9537c161ecca6f1
monica <mogimartinez@gmail.com>
2019-06-15 13:28:48 -0500

    ready for heroku

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var parseString = require('xml2js').parseString;
-const util = require('util');
-const axios = require('axios')
+
+
 
 
 // Serve static content for the app from the "public" directory in the application directory.
@@ -46,81 +45,31 @@ require("./controllers/routes")(app);
 
 // });
 
-// app.get('/new', function (req, res) {
+app.get('/new', function (req, res) {
 
-// const xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
 
-// xhr.onreadystatechange = function () {
-//     if (xhr.readyState == 4) {
-//         if (xhr.status === 200) {
-//             //   console.log(xhr.responseText);
-//             //   res.send(xhr.responseText)
-//             parseString(xhr.responseText, function (err, result) {
-//                 // console.dir(result.search);
-//                 console.log(util.inspect(result.GoodreadsResponse.search, false, null));
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+        if (xhr.status === 200) {
+            //   console.log(xhr.responseText);
+            //   res.send(xhr.responseText)
+            parseString(xhr.responseText, function (err, result) {
+                // console.dir(result.search);
+                console.log(util.inspect(result.GoodreadsResponse.search, false, null));
 
-//             });
-//         }
-//         if (xhr.status == 404) {
-//             console.log('File not found')
-//         }
-//     }
-// };
-// xhr.open('get', 'https://www.goodreads.com/search/index.xml?key=x3HAWeRhYJyCzsHPwGWgA&q=the+hobbit', true);
-// xhr.send();
+            });
+        }
+        if (xhr.status == 404) {
+            console.log('File not found')
+        }
+    }
+};
+xhr.open('get', 'https://www.goodreads.com/search/index.xml?key=x3HAWeRhYJyCzsHPwGWgA&q=the+hobbit', true);
+xhr.send();
 
-// })
+})
 
-// app.get('/dos', function (req, res) {
-//     function searchMovies() {
-//         const title = req.query.search;
-
-//         axios.get("http://www.omdbapi.com/?t=" + title + "&apikey=" + omdb.id)
-//             .then(
-//                 function (response) {
-//                     // console.log(response.data);
-//                     if (!userInput) {
-//                         // console.log("testing testing")
-//                         axios.get("http://www.omdbapi.com/?t=inception&apikey=" + omdb.id)
-//                             .then(
-//                                 function (response) {
-//                                     // console.log(response.data);
-//                                     if (!userInput) {
-//                                         // console.log("this is a movie")
-//                                         console.log("\nNo movie input given.\nHere is some info about my favorite movie (:\n")
-//                                         const movieInfo = response.data;
-//                                         // console.log("Title: " + movieInfo.Title);
-//                                         console.log(`TITLE: ${movieInfo.Title}`);
-//                                         console.log(`RELEASE YEAR: ${movieInfo.Year}`);
-//                                         console.log(`ACTORS: ${movieInfo.Actors}`);
-//                                         console.log(`PLOT: ${movieInfo.Plot}`);
-//                                         console.log(`LANGUAGE: ${movieInfo.Language}`);
-//                                         console.log(`PRODUCED IN: ${movieInfo.Country}`);
-//                                         console.log(`IMDB RATING: ${movieInfo.imdbRating}`);
-//                                         console.log(`ROTTEN TOMATOES: ${movieInfo.Ratings[1].Value}`);
-//                                     }
-//                                 })
-//                             .catch(function (error) {
-//                                 console.log(error);
-//                             });
-//                     } else {
-//                         const movieInfo = response.data;
-//                         // console.log("Title: " + movieInfo.Title);
-//                         console.log(`TITLE: ${movieInfo.Title}`);
-//                         console.log(`RELEASE YEAR: ${movieInfo.Year}`);
-//                         console.log(`ACTORS: ${movieInfo.Actors}`);
-//                         console.log(`PLOT: ${movieInfo.Plot}`);
-//                         console.log(`LANGUAGE: ${movieInfo.Language}`);
-//                         console.log(`PRODUCED IN: ${movieInfo.Country}`);
-//                         console.log(`IMDB RATING: ${movieInfo.imdbRating}`);
-//                         console.log(`ROTTEN TOMATOES: ${movieInfo.Ratings[1].Value}`);
-//                     }
-//                 })
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     }
-// });
 
 
 //XML GET request to goodreads api
