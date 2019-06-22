4f1d0912645f3ddf72eb2100c1576be9ad8cf0b3
monica <mogimartinez@gmail.com>
2019-06-21 23:18:37 -0500

    added signin/signout

--- server.js
+++ server.js
@@ -1,93 +1,69 @@
 //---- REQUIRE .env ----
 require("dotenv").config();
+const express = require("express");
+const app = express();
 
-var express = require("express");
+//Handle Authentication
+//==========================================================================
+const passport = require('passport');
+const session = require("express-session")
+//==========================================================================
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
-// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+const connection = require('./config/connection');
+app.set("view engine", "ejs");
 
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
+
+// Extracts body of incomin request and exposes in format easier to work with(JSON)
+//==========================================================================
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
+//==========================================================================
 
-app.set("view engine", "ejs");
 
+// Initialize Passport/passport session/express seassion and adds them as middleware
+//==========================================================================
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+//==========================================================================
+
+
+//Models
+//==========================================================================
+var models = require("./models");
+//==========================================================================
+
+
+//Routes
 require("./controllers/routes")(app);
-
-//ROUTE
-// ===================================================================
-// app.get('/results', function (req, res) {
-//     //  console.log(req.query.search)
-//     let query = req.query.search;
-//     let url = 'https://api.nytimes.com/svc/books/v3/reviews.json?api-key=Mq5lFkaIzR9btltiM1AWfSABUqOGAeRr&author=' + query;
+let authRoute = require('./controllers/auth')(app, passport);
 
 
-
-//     request(url, function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             //To convert body to object
-//             let data = JSON.parse(body)
-//             res.render('results', {
-//                 bookData: data
-//             });
-//         }
-//     });
-
-// });
-
-app.get('/new', function (req, res) {
-
-const xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4) {
-        if (xhr.status === 200) {
-            //   console.log(xhr.responseText);
-            //   res.send(xhr.responseText)
-            parseString(xhr.responseText, function (err, result) {
-                // console.dir(result.search);
-                console.log(util.inspect(result.GoodreadsResponse.search, false, null));
-
-            });
-        }
-        if (xhr.status == 404) {
-            console.log('File not found')
-        }
-    }
-};
-xhr.open('get', 'https://www.goodreads.com/search/index.xml?key=x3HAWeRhYJyCzsHPwGWgA&q=the+hobbit', true);
-xhr.send();
-
-})
+//load passport strategies
+require('./config/passport/passport')(passport, models.user);
 
 
-
-//XML GET request to goodreads api
-// ===================================================================
-// const xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = function () {
-//   if (xhr.readyState == 4){
-//     if (xhr.status === 200) {
-//       console.log(xhr.responseText);
-//       res.send(xhr.responseText)
-//     }
-//     if(xhr.status == 404){
-//       console.log('File not found')
-//     }
-//   }
-// };
-// xhr.open('get', 'https://www.goodreads.com/search/index.xml?key=x3HAWeRhYJyCzsHPwGWgA&q=the+hobbit', true);
-// xhr.send();
-
-
+//Sync Database
+//==========================================================================
+models.sequelize.sync().then(function () {
+    console.log('Nice! Database looks fine')
+}).catch(function (err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+//==========================================================================
 
 
 // Start our server so that it can begin listening to client requests.
