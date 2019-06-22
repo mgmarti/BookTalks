d6e7ac76e5e89362da72b9927156a562e0b7d165
monica <mogimartinez@gmail.com>
2019-06-21 23:35:48 -0500

    deploying to use mysql

--- config/connection.js
+++ config/connection.js
@@ -3,26 +3,19 @@
 ==================================*/
 
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection(process.env.JAWSDB_URL ||{
-    host: 'localhost',
-    user: 'root',
-    password: process.env.SQL_KEY,
-    database: 'booktalks'
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL || );
-// } else {
-//     connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'root',
-//         database: 'tacos_db'
-
-
-//     });
-// };
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'booktalks'
+    })
+}
 
 connection.connect(function (err) {
     console.log("Connected as id: " + connection.threadId);
