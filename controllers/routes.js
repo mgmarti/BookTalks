246792f627661c6275f8392cb885507c598ba031
monica <mogimartinez@gmail.com>
2019-06-22 09:21:07 -0500

    css for button and chat added

--- controllers/routes.js
+++ controllers/routes.js
@@ -52,6 +52,9 @@ module.exports = function (app) {
             });
     });
 
+    app.get('/chat', function(req,res){
+        res.render('chat')
+    })
 
     app.get('/signup', authController.signup);
 
